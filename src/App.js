7e9e2f3d3791eb26986f78c2d5134a28242fb4e9
羅中廷 <d857700@gmail.com>
2020-09-24 15:06:20 +0800

    init

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ import { DropdownCard, DropdownItem, DropdownMenu, DropdownList } from './compon
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Card } from 'react-bootstrap';
+import Shop from './pages/Shop';
 
 const Header=()=>(
-  <div class="header-section">
+  <header id="header" className="header">
+  <div className="header-section">
   <div className="container header-hide-content pt-2">
   <div className="d-flex align-items-center">
     {/*  Language */}
@@ -28,9 +30,9 @@ const Header=()=>(
               <DropdownItem>聯絡資訊</DropdownItem>
             </DropdownMenu>
             {/* Links */}
-            <div class="nav nav-sm nav-y-0 d-none d-sm-flex ml-sm-auto">
-              <a class="nav-link" href="../pages/faq.html">幫助</a>
-              <a class="nav-link" href="../pages/contacts-agency.html">聯絡資訊</a>
+            <div className="nav nav-sm nav-y-0 d-none d-sm-flex ml-sm-auto">
+              <a className="nav-link" href="../pages/faq.html">幫助</a>
+              <a className="nav-link" href="../pages/contacts-agency.html">聯絡資訊</a>
             </div>
           </div>
           <ul className="list-inline ml-2 mb-0">
@@ -84,21 +86,249 @@ const Header=()=>(
           <a className="navbar-brand" href="/">
             <img src="/logo.png" alt="Logo" />
           </a>
-          </nav>
+          <button type="button" className="navbar-toggler btn btn-icon btn-sm rounded-circle"
+                  aria-label="Toggle navigation"
+                  aria-expanded="false"
+                  aria-controls="navBar"
+                  data-toggle="collapse"
+                  data-target="#navBar">
+            <span className="navbar-toggler-default">
+              <svg width="14" height="14" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                <path fill="currentColor" d="M17.4,6.2H0.6C0.3,6.2,0,5.9,0,5.5V4.1c0-0.4,0.3-0.7,0.6-0.7h16.9c0.3,0,0.6,0.3,0.6,0.7v1.4C18,5.9,17.7,6.2,17.4,6.2z M17.4,14.1H0.6c-0.3,0-0.6-0.3-0.6-0.7V12c0-0.4,0.3-0.7,0.6-0.7h16.9c0.3,0,0.6,0.3,0.6,0.7v1.4C18,13.7,17.7,14.1,17.4,14.1z"/>
+              </svg>
+            </span>
+            <span className="navbar-toggler-toggled">
+              <svg width="14" height="14" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                <path fill="currentColor" d="M11.5,9.5l5-5c0.2-0.2,0.2-0.6-0.1-0.9l-1-1c-0.3-0.3-0.7-0.3-0.9-0.1l-5,5l-5-5C4.3,2.3,3.9,2.4,3.6,2.6l-1,1 C2.4,3.9,2.3,4.3,2.5,4.5l5,5l-5,5c-0.2,0.2-0.2,0.6,0.1,0.9l1,1c0.3,0.3,0.7,0.3,0.9,0.1l5-5l5,5c0.2,0.2,0.6,0.2,0.9-0.1l1-1 c0.3-0.3,0.3-0.7,0.1-0.9L11.5,9.5z"/>
+              </svg>
+            </span>
+          </button>
+          <div id="navBar" className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+            <li className="navbar-nav-item">
+                <Link className="nav-link">農產品介紹</Link>
+              </li>
+              <li className="navbar-nav-item">
+                <Link className=" nav-link">區塊鏈溯源</Link>
+              </li>
+              <li className="navbar-nav-last-item">
+                <a className="btn btn-sm btn-success transition-3d-hover" href="https://themes.getbootstrap.com/product/front-multipurpose-responsive-template/" target="_blank">
+                  開始使用
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
   </div>
   </div>
+  </header>
 )
 
-const Footer=()=>{
-  
-}
+const Footer=()=>(
+  <footer className="border-top">
+  <div className="container">
+    <div className="row justify-content-lg-between space-top-2 space-bottom-lg-2">
+      <div className="col-lg-3 mb-5">
+        <div className="d-flex align-items-start flex-column h-100">
+          <a className="w-100 mb-3 mb-lg-auto" href="../landings/index.html" aria-label="Front">
+            <img className="brand" src="/logo.png" alt="Logo" />
+          </a>
+          <p className="small text-muted mb-0">&copy; 千農科技有限公司</p>
+        </div>
+      </div>
+      <div className="col-6 col-md-4 col-lg-3 ml-lg-auto mb-5 mb-lg-0">
+        <h5>Account</h5>
+        <ul className="nav nav-sm nav-x-0 flex-column">
+          <li className="nav-item"><a className="nav-link" href="#">Placing an order</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Shipping options</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Tracking a package</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Country availability</a></li>
+        </ul>
+      </div>
+      <div className="col-6 col-md-4 col-lg-3 mb-5 mb-lg-0">
+        <h5>Company</h5>
+        <ul className="nav nav-sm nav-x-0 flex-column">
+          <li className="nav-item"><a className="nav-link" href="#">Financing</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Recycling</a></li>
+          <li className="nav-item"><a className="nav-link" href="#">Return policy</a></li>
+        </ul>
+      </div>
+      <div className="col-md-4 col-lg-2 mb-5 mb-lg-0">
+        <h5>Our Location</h5>
+        <ul className="nav nav-sm nav-x-0 flex-column">
+          <li className="nav-item">
+            <a className="nav-link" href="../help-desk/index.html">
+              <span className="media align-items-center">
+                <i className="fas fa-info-circle mr-2"></i>
+                <span className="media-body">Help</span>
+              </span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <span className="media align-items-center">
+                <i className="fas fa-user-circle mr-2"></i>
+                <span className="media-body">Your Account</span>
+              </span>
+            </a>
+          </li>
+          <li className="position-relative">
+            <div className="hs-unfold position-static">
+              <a className="js-hs-unfold-invoker nav-link" href="javascript:;"
+                 >
+                <img className="dropdown-item-icon" src="https://upload.wikimedia.org/wikipedia/commons/7/72/Flag_of_the_Republic_of_China.svg" alt="United States Flag" />
+                <span>台灣</span>
+              </a>
+
+              <div id="footerCountry" className="hs-unfold-content dropdown-menu dropdown-card dropdown-menu-md-right dropdown-menu-bottom w-100 w-sm-auto mb-0">
+                <div className="card">
+                  <div className="card-body">
+                    <h5>Front available in</h5>
+                    <div className="row">
+                      <div className="col-6">
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/au.svg" alt="Australia Flag"/>
+                          Australia
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/at.svg" alt="Austria Flag"/>
+                          Austria
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/be.svg" alt="Belgium Flag"/>
+                          Belgium
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/ca.svg" alt="Canada Flag"/>
+                          Canada
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/dk.svg" alt="Denmark Flag" />
+                          Denmark
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/fi.svg" alt="Finland Flag"/>
+                          Finland
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/fr.svg" alt="France Flag"/>
+                          France
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/de.svg" alt="Germany Flag"/>
+                          Germany
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/nl.svg" alt="Netherlands Flag"/>
+                          Netherlands
+                        </a>
+                      </div>
+                      <div className="col-6">
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/nz.svg" alt="New Zealand Flag"/>
+                          New Zealand
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/no.svg" alt="Norway Flag"/>
+                          Norway
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/pt.svg" alt="PortugalPREVIEW Flag"/>
+                          Portugal
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/sg.svg" alt="Singapore Flag"/>
+                          Singapore
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/es.svg" alt="Spain Flag"/>
+                          Spain
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/se.svg" alt="Sweden Flag"/>
+                          Sweden
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/ch.svg" alt="Switzerland Flag"/>
+                          Switzerland
+                        </a>
+                        <a className="nav-link" href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/gb.svg" alt="United Kingdom Flag"/>
+                          UK
+                        </a>
+                        <a className="nav-link active " href="#">
+                          <img className="max-w-3rem mr-1" src="../../assets/vendor/flag-icon-css/flags/4x3/us.svg" alt="United States Flag"/>
+                          US
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <a className="card-footer border-0 card-bg-light" href="#">
+                    <span className="d-block text-muted mb-1">More countries coming soon.</span>
+                    <span className="d-block font-size-1">Signup to get notified <i className="fas fa-angle-right fa-sm ml-1"></i></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <hr className="my-0" />
+
+    <div className="row align-items-md-center space-1">
+      <div className="col-md-4 mb-4 mb-md-0">
+        <ul className="list-inline mb-0">
+          <li className="list-inline-item">
+            <a className="btn btn-xs btn-icon btn-soft-secondary" href="#">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn btn-xs btn-icon btn-soft-secondary" href="#">
+              <i className="fab fa-google"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn btn-xs btn-icon btn-soft-secondary" href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn btn-xs btn-icon btn-soft-secondary" href="#">
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col-md-8 text-md-right">
+        <ul className="nav nav-sm justify-content-md-end">
+          <li className="nav-item">
+            <a className="nav-link pl-0" href="../pages/privacy.html">Privacy &amp; policy</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="../pages/terms.html">Terms &amp; conditions</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+)
 
 function App() {
   return (
-    <>
+    <Router>
+
     <Header />
+    <Switch>
+          <Route path="/"><Shop /></Route>
+          <Route path="/"><Home /></Route>
+    </Switch>
     <Footer />
-    </>
+    </Router>
   );
 }
 
