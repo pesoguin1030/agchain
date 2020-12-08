50625bb47a5ddabe24199462e875e5045d9b82f8
105060007 <cctiger123@gmail.com>
2020-12-08 22:41:26 +0800

    修改雷達圖

--- src/components/RadarChart/index.js
+++ src/components/RadarChart/index.js
@@ -7,7 +7,7 @@ const state = {
     {
       label: "Rainfall",
       backgroundColor: "rgba(75,192,192,0.2)",
-      borderColor: "rgba(200, 99, 132, .7)",
+      borderColor: "rgba(75,192,192, 0.9)",
       borderWidth: 2,
       data: [65, 59, 80, 81],
     },
