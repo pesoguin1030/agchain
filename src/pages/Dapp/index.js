09e468c2f0bbc29a08d07c5848bdce5eaeebb3fd
105060007 <cctiger123@gmail.com>
2020-12-12 23:33:51 +0800

    過濾掉同一個TxHash的田間紀錄 新增一個區塊(?)

--- src/pages/Dapp/index.js
+++ src/pages/Dapp/index.js
@@ -73,11 +73,19 @@ function Dapp(props) {
     return objs;
   }
 
-  function sortCultivationRecord(response) {
-    response.sort(function (a, b) {
+  function sortCultivationRecord(records) {
+    // 過濾掉同一個TxHash的田間紀錄
+    let uniqueRecords = Array.from(new Set(records.map((a) => a.txHash))).map(
+      (txHash) => {
+        return records.find((a) => a.txHash === txHash);
+      }
+    );
+    // 依時間先後順序排列
+    uniqueRecords.sort(function (a, b) {
       return a.timestamp - b.timestamp;
     });
-    return response;
+
+    return uniqueRecords;
   }
 
   async function setupRequiredInformation(traceID) {
@@ -238,8 +246,21 @@ function Dapp(props) {
         </div>
       ) : null}
       <div className="container space-2 space-lg-3">
+        <div className="w-md-80 w-lg-40 text-center mx-md-auto mb-5">
+          <img
+            className="img-fluid w-md-50 w-lg-40 mx-7 my-3"
+            src="/assets/svg/icons/icon-50.svg"
+            alt="SVG"
+          />
+          <h1>區塊鏈信賴溯源</h1>
+          <p style={{ letterSpacing: "0.2rem" }}>
+            以下資料皆有區塊鏈加密與不可竄改之信賴保障，提高食品安全透明度。
+          </p>
+        </div>
+      </div>
+      <div className="container space-2 space-lg-3">
         <div className="w-md-80 w-lg-40 text-center mx-md-auto mb-5 mb-md-9">
-          <h2>出貨前照片</h2>
+          <h2>防偽鑑識照片</h2>
         </div>
         <div className="row w-md-80 w-lg-40 text-center mx-md-auto mb-5 mb-md-9">
           {secureItem && secureItem.cid !== "" ? (
@@ -259,7 +280,7 @@ function Dapp(props) {
 
       <div className="container space-2 space-lg-3">
         <div className="w-md-80 w-lg-40 text-center mx-md-auto mb-5 mb-md-9">
-          <h2>區塊鏈信賴溯源</h2>
+          <h2>田間紀錄</h2>
         </div>
         <div className="row w-md-80 w-lg-60 mx-md-auto px-5">
           {cultivationRecord && cultivationRecord.length !== 0 ? (
