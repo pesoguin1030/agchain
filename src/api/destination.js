1d25b30f2be3bbcb5bf3e6a4523dbed7304fabdc
邵嘉嶸 <s9755059@gmail.com>
2021-04-25 16:21:39 +0800

    fix very basic destination problem

--- src/api/destination.js
+++ src/api/destination.js
@@ -3,7 +3,7 @@ import Constants from "./constants";
 
 const fetchDestination = async () => {
   try {
-    const { data } = await request.get(`/destination`);
+    const { data } = await request.get(`/destination/oldAddress`);
     return data;
   } catch (err) {
     return Promise.reject(err);
