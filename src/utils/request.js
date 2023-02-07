07aef1d1537cfda3d84c576f30505de50a878f21
KatyushaScarlet <katyushascarlet@outlook.com>
2023-02-07 21:12:49 +0800

    Restore the url

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 // import {} from "../appContext";
 // Modify server url here
-const baseURL = "http://localhost:4000";
-// const baseURL = "https://app.freshio.me";
+// const baseURL = "http://localhost:4000";
+const baseURL = "https://app.freshio.me";
 const request = axios.create({
   baseURL: baseURL,
   withCredentials: true, //允许携带cookie，后端必须设置跨域CORS规则，且不能用*
